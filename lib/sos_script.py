020e1f4f0edf2a02aa438bb6f0e64419fff6a9bf
George Kraft <cynerva@gmail.com>
2016-11-10 11:44:28 -0600

    flake8

--- lib/sos_script.py
+++ lib/sos_script.py
@@ -2,6 +2,7 @@ import os
 
 dir = os.environ["SOS_SCRIPT_DIR"]
 
+
 def open_file(path, *args, **kwargs):
-  """ Open a file within the SOS script dir """
-  return open(os.path.join(dir, path), *args, **kwargs)
+    """ Open a file within the SOS script dir """
+    return open(os.path.join(dir, path), *args, **kwargs)
